f5019b87e9247e5c263b440f69c220d888191703
HwasooSung <hwasu92@naver.com>
2016-04-26 23:57:26 +0900

    review module.exports

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
+'use strict';
+
 var express = require('express')
   , bodyParser = require('body-parser')
-  , routes = require('./lib/route/posts')
+  , routes = require('./lib/routes/all')
   ;
-
+  
 var app = express();
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
 
 app.use('/api', routes);
 
-app.get('/', function (req, res) {
-  res.send("Hello This is my 1st page");
-});
-
-exports.app = app;
-
+  
 app.listen(8888, function () {
-  console.log("Listening on 8888");
-});
+    console.log("listening on 8888");
+    });
